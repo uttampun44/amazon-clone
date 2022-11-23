a484f7c19f17b2f19145246ccfda1bb54b0f9b09
uttampun <uttampun62@gmail.com>
2022-11-23 08:48:13 +0545

    box shadow added

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-  <Auth0Provider
-  domain="dev-hzd5efyddr631tz6.us.auth0.com"
-  clientId="awKuNDEbouftP6tjAH7GUzIwarx4NyYA"
-  redirectUri={window.location.origin}
->
    <App />
-   </Auth0Provider>,
   </React.StrictMode>
 );
 
